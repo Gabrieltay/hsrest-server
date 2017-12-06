ce0492d1f914486b53ccc2484e28db2e49bd9c84
Lup Peng <luppeng@Lups-MacBook-Pro.local>
2017-12-06 11:54:25 +0800

    Added user friendly welcome msg

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('port', (process.env.PORT || 8080));
 
 
 app.get('/', function (req, res) {
-   res.send("Hello World");
+   res.send("Welcome to bgphs Pseudo API server");
 })
 
 var errorCodeMessageMap = { 200: "",
