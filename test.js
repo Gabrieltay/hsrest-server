e9df5b9d0df7d4aa2610722fcd395fb5ffb9d72c
Lup Peng <luppeng@Lups-MacBook-Pro.local>
2017-12-06 11:44:56 +0800

    Adding test files

---
+++ test.js
@@ -0,0 +1,114 @@
+// Require 
+var http = require('http')
+var testObjects = require('./testObjects.js')
+
+// Server Properties 
+var serverUrl = "bgphs-server.herokuapp.com"
+var grantInfoEndpoint = "/grantinfo"
+var riskreportEndpoint = "/riskreport"
+var watchblacklistEndpoint = "/watchblacklist"
+var testUrlResource = "/item1"
+
+// Expected Error Responses
+var error400Response = { statusCode: 400, body: { dataError: 'Request data field Error' } };
+var error401Response = { statusCode: 401, body: { dataError: 'Authentication Failure' } };
+var error404Response = { statusCode: 404, body: { dataError: 'Not Found' } };
+var error407Response = { statusCode: 407, body: { dataError: 'Authentication Failure' } };
+var error422Response = { statusCode: 422, body: { dataError: 'Invalid startDate format (hardcoded attribute for this errorCode)' } };
+var error504Response = { statusCode: 504, body: { dataError: 'Request Timeout' } };
+
+function testEndpoint(httpMethod, endpoint, requestBody, responseCheck, description) {
+    var options = {  hostname: serverUrl,
+                     path: endpoint, 
+                     method: httpMethod,
+                     headers: { 'Content-Type': 'application/json' }
+                  }
+
+    var req = http.request(options, function(response) {
+        var dataString = "";
+
+        response.on("data", function(data) { 
+            dataString += data; 
+        });
+
+        response.on("end", function () {
+            if (JSON.stringify(JSON.parse(dataString)) === JSON.stringify(responseCheck) ) {
+                console.log("Test for " + endpoint + " endpoint: " + description + " " + httpMethod + " Successful!");
+                return true;
+            } else {
+                console.log("Test for " + endpoint + " endpoint: " + description + " " + httpMethod + " FAILED!");
+                return false;
+            }
+        });
+    });
+
+    req.on('error', function(error) { 
+        console.log("Test for " + endpoint + " endpoint: " + description + " " +  httpMethod + " FAILED! (request encountered an error)");
+        console.log(error);
+        return false;
+    });
+
+    req.write( JSON.stringify(requestBody) );
+    req.end();
+
+}
+
+// Grant Info Endpoint - POST
+testEndpoint('POST', grantInfoEndpoint, testObjects['grantInfoCorrectPostReq'], testObjects['grantInfoCorrectPostRes'], 'Well Formed Request')
+testEndpoint('POST', grantInfoEndpoint, testObjects['grantInfoWrongPostReq'], testObjects['grantInfoWrongPostRes'], 'Malformed Request')
+testEndpoint('POST', grantInfoEndpoint, {'errorCode': 400}, error400Response, 'Custom Error 400');
+testEndpoint('POST', grantInfoEndpoint, {'errorCode': 401}, error401Response, 'Custom Error 401');
+testEndpoint('POST', grantInfoEndpoint, {'errorCode': 404}, error404Response, 'Custom Error 404');
+testEndpoint('POST', grantInfoEndpoint, {'errorCode': 407}, error407Response, 'Custom Error 407');
+testEndpoint('POST', grantInfoEndpoint, {'errorCode': 422}, error422Response, 'Custom Error 422');
+testEndpoint('POST', grantInfoEndpoint, {'errorCode': 504}, error504Response, 'Custom Error 504');
+
+// Grant Info Endpoint - PUT
+testEndpoint('PUT', grantInfoEndpoint + testUrlResource, testObjects['grantInfoCorrectPutReq'], testObjects['grantInfoCorrectPutRes'], 'Well Formed Request')
+testEndpoint('PUT', grantInfoEndpoint + testUrlResource, testObjects['grantInfoWrongPutReq'], testObjects['grantInfoWrongPutRes'], 'Malformed Request')
+testEndpoint('PUT', grantInfoEndpoint + testUrlResource, {'errorCode': 400}, error400Response, 'Custom Error 400');
+testEndpoint('PUT', grantInfoEndpoint + testUrlResource, {'errorCode': 401}, error401Response, 'Custom Error 401');
+testEndpoint('PUT', grantInfoEndpoint + testUrlResource, {'errorCode': 404}, error404Response, 'Custom Error 404');
+testEndpoint('PUT', grantInfoEndpoint + testUrlResource, {'errorCode': 407}, error407Response, 'Custom Error 407');
+testEndpoint('PUT', grantInfoEndpoint + testUrlResource, {'errorCode': 422}, error422Response, 'Custom Error 422');
+testEndpoint('PUT', grantInfoEndpoint + testUrlResource, {'errorCode': 504}, error504Response, 'Custom Error 504');
+
+// Risk Report Adhoc Endpoint - POST 
+testEndpoint('POST', riskreportEndpoint + "/adhoc", testObjects['riskReportAdhocCorrectPostReq'], testObjects['riskReportAdhocCorrectPostRes'], 'Well Formed Request')
+testEndpoint('POST', riskreportEndpoint + "/adhoc", testObjects['riskReportAdhocWrongPostReq'], testObjects['riskReportAdhocWrongPostRes'], 'Malformed Request')
+testEndpoint('POST', riskreportEndpoint + "/adhoc", {'errorCode': 400}, error400Response, 'Custom Error 400');
+testEndpoint('POST', riskreportEndpoint + "/adhoc", {'errorCode': 401}, error401Response, 'Custom Error 401');
+testEndpoint('POST', riskreportEndpoint + "/adhoc", {'errorCode': 404}, error404Response, 'Custom Error 404');
+testEndpoint('POST', riskreportEndpoint + "/adhoc", {'errorCode': 407}, error407Response, 'Custom Error 407');
+testEndpoint('POST', riskreportEndpoint + "/adhoc", {'errorCode': 422}, error422Response, 'Custom Error 422');
+testEndpoint('POST', riskreportEndpoint + "/adhoc", {'errorCode': 504}, error504Response, 'Custom Error 504');
+
+// Risk Report Detailed Endpoint - POST 
+testEndpoint('POST', riskreportEndpoint + "/detailed", testObjects['riskReportDetailedCorrectPostReq'], testObjects['riskReportDetailedCorrectPostRes'], 'Well Formed Request')
+testEndpoint('POST', riskreportEndpoint + "/detailed", testObjects['riskReportDetailedWrongPostReq'], testObjects['riskReportDetailedWrongPostRes'], 'Malformed Request')
+testEndpoint('POST', riskreportEndpoint + "/detailed", {'errorCode': 400}, error400Response, 'Custom Error 400');
+testEndpoint('POST', riskreportEndpoint + "/detailed", {'errorCode': 401}, error401Response, 'Custom Error 401');
+testEndpoint('POST', riskreportEndpoint + "/detailed", {'errorCode': 404}, error404Response, 'Custom Error 404');
+testEndpoint('POST', riskreportEndpoint + "/detailed", {'errorCode': 407}, error407Response, 'Custom Error 407');
+testEndpoint('POST', riskreportEndpoint + "/detailed", {'errorCode': 422}, error422Response, 'Custom Error 422');
+testEndpoint('POST', riskreportEndpoint + "/detailed", {'errorCode': 504}, error504Response, 'Custom Error 504');
+
+// Watch/Black List Endpoint - POST 
+testEndpoint('POST', watchblacklistEndpoint, testObjects['listCorrectPostReq'], testObjects['listCorrectPostRes'], 'Well Formed Request')
+testEndpoint('POST', watchblacklistEndpoint, testObjects['listWrongPostReq'], testObjects['listWrongPostRes'], 'Malformed Request')
+testEndpoint('POST', watchblacklistEndpoint, {'errorCode': 400}, error400Response, 'Custom Error 400');
+testEndpoint('POST', watchblacklistEndpoint, {'errorCode': 401}, error401Response, 'Custom Error 401');
+testEndpoint('POST', watchblacklistEndpoint, {'errorCode': 404}, error404Response, 'Custom Error 404');
+testEndpoint('POST', watchblacklistEndpoint, {'errorCode': 407}, error407Response, 'Custom Error 407');
+testEndpoint('POST', watchblacklistEndpoint, {'errorCode': 422}, error422Response, 'Custom Error 422');
+testEndpoint('POST', watchblacklistEndpoint, {'errorCode': 504}, error504Response, 'Custom Error 504');
+
+// Watch/Black List Endpoint - PUT
+testEndpoint('PUT', watchblacklistEndpoint + testUrlResource, testObjects['listCorrectPutReq'], testObjects['listCorrectPutRes'], 'Well Formed Request')
+testEndpoint('PUT', watchblacklistEndpoint + testUrlResource, testObjects['listWrongPutReq'], testObjects['listWrongPutRes'], 'Malformed Request')
+testEndpoint('PUT', watchblacklistEndpoint + testUrlResource, {'errorCode': 400}, error400Response, 'Custom Error 400');
+testEndpoint('PUT', watchblacklistEndpoint + testUrlResource, {'errorCode': 401}, error401Response, 'Custom Error 401');
+testEndpoint('PUT', watchblacklistEndpoint + testUrlResource, {'errorCode': 404}, error404Response, 'Custom Error 404');
+testEndpoint('PUT', watchblacklistEndpoint + testUrlResource, {'errorCode': 407}, error407Response, 'Custom Error 407');
+testEndpoint('PUT', watchblacklistEndpoint + testUrlResource, {'errorCode': 422}, error422Response, 'Custom Error 422');
+testEndpoint('PUT', watchblacklistEndpoint + testUrlResource, {'errorCode': 504}, error504Response, 'Custom Error 504');
---
+++ testObjects.js
@@ -0,0 +1,437 @@
+var testObjects = {
+
+    // POST /grantinfo
+    "grantInfoCorrectPostReq": {
+        "transactionID": "1501AB24",
+        "transactionTime": "2017-12-05T16:23:54.172Z",
+        "applicationInfo": {
+            "applicationID": "1501AB24",
+            "userNric": "S1234567A",
+            "userName": "Tan Ah Seng",
+            "industryType": "Engineering Services",
+            "developmentCategory": "Capability Development",
+            "functionalArea": "IT Software",
+            "subFunctionalArea": "ICT Productivity Solutions",
+            "applicationStatus": "A2"
+        },
+        "applicationContactInfo": {
+            "contactPersonName": "Person A",
+            "contactPersonDesignation": "CEO",
+            "contactPersonPhone": "81231234",
+            "contactPersonEmail": "person_a@testing.com.sg",
+            "contactPersonSecondaryEmail": "person_a@other-email.com",
+            "correspondenceAddress": {
+                "addressType": null,
+                "foreignAddress1": null,
+                "foreignAddress2": null,
+                "block": "678",
+                "street": "Garlick Avenue",
+                "level": "70",
+                "buildingName": null,
+                "unit": "70",
+                "postalCode": "386018"
+            },
+            "lofAddresseeName": "Person B",
+            "lofAddresseeDesignation": "CEO",
+            "lofAddresseeEmail": "person_b@testedtrading.com.sg"
+        },
+        "applicationLocationDeployed": {
+            "locationDeployedType": "Home Office",
+            "locationDeployedProjectAddress": null,
+            "locationDeployedAddress": {
+                "addressType": null,
+                "foreignAddress1": null,
+                "foreignAddress2": null,
+                "block": "678",
+                "street": "Garlick Avenue",
+                "level": "70",
+                "buildingName": null,
+                "unit": "70",
+                "postalCode": "386018"
+            }
+        },
+        "claimInfo": {
+            "applicationID": "1501AB24",
+            "officerID": "processing.officer@example.com",
+            "officerName": "Tan Ah Bee",
+            "approvedGrantAmount": "1000000000"
+        },
+        "claimContactInfo": {
+            "contactPersonName": "Person A",
+            "contactPersonDesignation": "CEO",
+            "contactPersonPhone": "81231234",
+            "contactPersonEmail": "person_a@testing.com.sg",
+            "contactPersonSecondaryEmail": "person_a@other-email.com",
+            "correspondenceAddress": {
+                "addressType": null,
+                "foreignAddress1": null,
+                "foreignAddress2": null,
+                "block": "678",
+                "street": "Garlick Avenue",
+                "level": "70",
+                "buildingName": null,
+                "unit": "70",
+                "postalCode": "386018"
+            }
+        },
+        "claimLocationDeployed": {
+            "locationDeployedType": "Home Office",
+            "locationDeployedProjectAddress": null,
+            "locationDeployedAddress": {
+                "addressType": null,
+                "foreignAddress1": null,
+                "foreignAddress2": null,
+                "block": "678",
+                "street": "Garlick Avenue",
+                "level": "70",
+                "buildingName": null,
+                "unit": "70",
+                "postalCode": "386018"
+            }
+        },
+        "companyGeneralInfo": {
+            "companyUEN": "663784266U"
+        },
+        "projectInfo": {
+            "projectTitle": "Purchase Software to Improve Productivity",
+            "startDate": "2017-12-05T16:23:54.172Z",
+            "endDate": "2017-12-05T16:23:54.172Z",
+            "costs": [{
+                    "type": "Consultancy and third party service provider",
+                    "vendorName": "Vendor A",
+                    "vendorUEN": "163784266U",
+                    "vendorSgRegisteredCheck": true
+                },
+                {
+                    "type": "Training provider",
+                    "vendorName": "Vendor B",
+                    "vendorUEN": "263784266U",
+                    "vendorSgRegisteredCheck": true
+                }
+            ]
+        }
+    },
+    "grantInfoCorrectPostRes": {
+        "statusCode": 200,
+        "body": {
+            "requestID": "1501AB24",
+            "dataError": ""
+        }
+    },
+
+    "grantInfoWrongPostReq": {
+        "transactionID": "1501AB24",
+        "transactionTime": "2017-12-05T16:23:54.172Z",
+        "applicationInfo": {
+            "applicationID": "1501AB24",
+            "userNric": "S1234567A",
+            "userName": "Tan Ah Seng",
+            "industryType": "Engineering Services",
+            "developmentCategory": "Capability Development",
+            "functionalArea": "IT Software",
+            "subFunctionalArea": "ICT Productivity Solutions",
+            "applicationStatus": "A2"
+        },
+        "applicationContactInfo": {
+            "contactPersonName": "Person A",
+            "contactPersonDesignation": "CEO",
+            "contactPersonPhone": "81231234",
+            "contactPersonEmail": "person_a@testing.com.sg",
+            "contactPersonSecondaryEmail": "person_a@other-email.com",
+            "correspondenceAddress": {
+                "addressType": null,
+                "foreignAddress1": null,
+                "foreignAddress2": null,
+                "block": "678",
+                "street": "Garlick Avenue",
+                "level": "70",
+                "buildingName": null,
+                "unit": "70",
+                "postalCode": "386018"
+            },
+            "lofAddresseeName": "Person B",
+            "lofAddresseeDesignation": "CEO",
+            "lofAddresseeEmail": "person_b@testedtrading.com.sg"
+        },
+        "applicationLocationDeployed": {
+            "locationDeployedType": "Home Office",
+            "locationDeployedProjectAddress": null,
+            "locationDeployedAddress": {
+                "addressType": null,
+                "foreignAddress1": null,
+                "foreignAddress2": null,
+                "block": "678",
+                "street": "Garlick Avenue",
+                "level": "70",
+                "buildingName": null,
+                "unit": "70",
+                "postalCode": "386018"
+            }
+        },
+        "claimInfo": {
+            "applicationID": "1501AB24",
+            "officerID": "processing.officer@example.com",
+            "officerName": "Tan Ah Bee",
+            "approvedGrantAmount": "1000000000"
+        },
+        "claimContactInfo": {
+            "contactPersonName": "Person A",
+            "contactPersonDesignation": "CEO",
+            "contactPersonPhone": "81231234",
+            "contactPersonEmail": "person_a@testing.com.sg",
+            "contactPersonSecondaryEmail": "person_a@other-email.com",
+            "correspondenceAddress": {
+                "addressType": null,
+                "foreignAddress1": null,
+                "foreignAddress2": null,
+                "block": "678",
+                "street": "Garlick Avenue",
+                "level": "70",
+                "buildingName": null,
+                "unit": "70",
+                "postalCode": "386018"
+            }
+        },
+        "claimLocationDeployed": {
+            "locationDeployedProjectAddress": null,
+            "locationDeployedAddress": {
+                "addressType": null,
+                "foreignAddress1": null,
+                "foreignAddress2": null,
+                "block": "678",
+                "street": "Garlick Avenue",
+                "level": "70",
+                "buildingName": null,
+                "unit": "70",
+                "postalCode": "386018"
+            }
+        },
+        "companyGeneralInfo": {
+            "companyUEN": "663784266U"
+        }
+    },
+    "grantInfoWrongPostRes": {
+        "statusCode": 400,
+        "body": {
+            "requestID": "1501AB24",
+            "dataError": "projectInfo, claimLocationDeployed.locationDeployedType attributes are required"
+        }
+    },
+
+    // PUT /grantinfo/:item
+    "grantInfoCorrectPutReq": {
+        "transactionID": "1501AB24",
+        "transactionTime": "2017-12-05T16:34:49.240Z",
+        "type": "updateOfficer",
+        "data": {
+            "role": "BGP BIM Grant Processing Officer",
+            "officerID": "7152",
+            "officerName": "John Tan"
+        }
+    },
+    "grantInfoCorrectPutRes": {
+        "statusCode": 200,
+        "body": {
+            "requestID": "1501AB24",
+            "dataError": ""
+        }
+    },
+
+    "grantInfoWrongPutReq": {
+        "transactionID": "1501AB24",
+        "transactionTime": "2017-12-05T16:34:49.240Z",
+        "type": "updateOfficer"
+    },
+    "grantInfoWrongPutRes": {
+        "statusCode": 400,
+        "body": {
+            "requestID": "1501AB24",
+            "dataError": "data attribute is required"
+        }
+    },
+
+    // POST /riskreport/adhoc
+    "riskReportAdhocCorrectPostReq": {
+        "transactionID": "1501AB24",
+        "transactionTime": "2017-12-05T16:49:16.343Z",
+        "applicationID": "1501AB"
+    },
+    "riskReportAdhocCorrectPostRes": {
+        "statusCode": 200,
+        "body": {
+            "requestID": "1501AB24",
+            "dataError": ""
+        }
+    },
+
+    "riskReportAdhocWrongPostReq": {
+        "transactionID": "1501AB24",
+        "transactionTime": "2017-12-05T16:49:16.343Z"
+    },
+    "riskReportAdhocWrongPostRes": {
+        "statusCode": 400,
+        "body": {
+            "requestID": "1501AB24",
+            "dataError": "applicationID attribute is required"
+        }
+    },
+
+    // POST /riskreport/detailed
+    "riskReportDetailedCorrectPostReq": {
+        "transactionID": "1501AB24",
+        "transactionTime": "2017-12-05T16:49:21.025Z",
+        "startDate": "2016-11-01T00:00:00.000Z",
+        "endDate": "2016-11-30T00:00:00.000Z"
+    },
+    "riskReportDetailedCorrectPostRes": {
+        "statusCode": 200,
+        "body": {
+            "requestID": "1501AB24",
+            "dataError": ""
+        }
+    },
+
+    "riskReportDetailedWrongPostReq": {
+        "transactionID": "1501AB24",
+        "endDate": "2016-11-30T00:00:00.000Z"
+    },
+    "riskReportDetailedWrongPostRes": {
+        "statusCode": 400,
+        "body": {
+            "requestID": "1501AB24",
+            "dataError": "transactionTime, startDate attributes are required"
+        }
+    },
+
+    // POST /watchblacklist
+    "listCorrectPostReq": {
+        "transactionID": "LIST45678",
+        "transactionTime": "2017-12-05T16:49:24.565Z",
+        "entityItemList": [{
+            "entityType": "company",
+            "entityName": "Vendor A Pte Ltd",
+            "entityID": "UEN456127",
+            "certificationNumber": null,
+            "blacklist": {
+                "agencyList": [
+                    "Agency1",
+                    "Agency2"
+                ],
+                "reason": "Persistently failed to comply with Section 2.4 of Vendor Act despite repeated warnings"
+            },
+            "watchlist": {
+                "agencyList": [
+                    "Agency1",
+                    "Agency2"
+                ],
+                "reason": "Persistently failed to comply with Section 2.4 of Vendor Act despite repeated warnings"
+            }
+        }]
+    },
+    "listCorrectPostRes": {
+	    "statusCode": 200,
+	    "body": {
+	        "requestID": "LIST45678",
+	        "dataError": ""
+	    }
+	},
+
+    "listWrongPostReq": {
+        "transactionID": "LIST45678",
+        "transactionTime": "2017-12-05T16:49:24.565Z",
+        "entityItemLista": [{
+            "entityType": "company",
+            "entityName": "Vendor A Pte Ltd",
+            "entityID": "UEN456127",
+            "certificationNumber": null,
+            "blacklist": {
+                "agencyList": [
+                    "Agency1",
+                    "Agency2"
+                ],
+                "reasonn": "Persistently failed to comply with Section 2.4 of Vendor Act despite repeated warnings"
+            },
+            "watchlistt": {
+                "agencyList": [
+                    "Agency1",
+                    "Agency2"
+                ],
+                "reason": "Persistently failed to comply with Section 2.4 of Vendor Act despite repeated warnings"
+            }
+        }]
+    },
+    "listWrongPostRes": {
+        "statusCode": 400,
+        "body": {
+            "requestID": "LIST45678",
+            "dataError": "entityItemList attribute is required"
+        }
+    },
+
+
+    // PUT /watchblacklist/:item
+    "listCorrectPutReq": {
+        "transactionID": "LIST45678",
+        "transactionTime": "2017-12-05T16:56:04.846Z",
+        "updateEntity": {
+            "entityType": "company",
+            "entityName": "Vendor A Pte Ltd",
+            "entityID": "UEN456127",
+            "certificationNumber": null,
+            "blacklist": {
+                "agencyList": [
+                    "Agency1",
+                    "Agency2"
+                ],
+                "reason": "Persistently failed to comply with Section 2.4 of Vendor Act despite repeated warnings"
+            },
+            "watchlist": {
+                "agencyList": [
+                    "Agency1",
+                    "Agency2"
+                ],
+                "reason": "Persistently failed to comply with Section 2.4 of Vendor Act despite repeated warnings"
+            }
+        }
+    },
+    "listCorrectPutRes": {
+        "statusCode": 200,
+        "body": {
+            "requestID": "LIST45678",
+            "dataError": ""
+        }
+    },
+
+    "listWrongPutReq": {
+        "transactionID": "LIST45678",
+        "transactionTime": "2017-12-05T16:56:04.846Z",
+        "updateEntity": {
+            "entityType": "company",
+            "entityName": "Vendor A Pte Ltd",
+            "entityID": "UEN456127",
+            "certificationNumber": null,
+            "blacklist": {
+                "agencyList": [
+                    "Agency1",
+                    "Agency2"
+                ]
+            },
+            "watchlist": {
+                "agencyListMispelt": [
+                    "Agency1",
+                    "Agency2"
+                ],
+                "reason": "Persistently failed to comply with Section 2.4 of Vendor Act despite repeated warnings"
+            }
+        }
+    },
+    "listWrongPutRes": {
+        "statusCode": 400,
+        "body": {
+            "requestID": "LIST45678",
+            "dataError": "updateEntity.blacklist.reason, updateEntity.watchlist.agencyList attributes are required"
+        }
+    }
+}
+
+module.exports = testObjects
